6045776c781a6d3471734f54e7d1ac2fdaf71912
alisha raghav <alisharaghavagra@gmail.com>
2023-09-11 08:02:46 +0530

    templates added

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index,name='index'),
     path('item/',views.item, name='item'),
+    path('<int:item_id>',views.detail,name='detail'),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 # Create your views here.
 def index(request):
-   return HttpResponse('hello world')
+   item_list= Item.objects.all()
+   template=loader.get_template('food/index.html')
+   context={
+      'item_list':item_list,
+   }
+   return render(request,'food/index.html')
+
 
 def item (request):
    return HttpResponse('this is an item view')
+
+def detail(request,item_id):
+   item= Item.objects.get(pk=item_id)
+   context={
+      'item':item,
+   }
+   return render(request,'food/detail.html,context')
