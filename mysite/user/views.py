d44852037a5efef1ba43c1527993b050ac3e1047
alisha raghav <alisharaghav09@gmail.com>
2023-09-25 22:57:38 +0530

    users reg

--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 # Create your views here.
 def register(request):
-    form=UserCreationForm()
-    return render (request,'user/register.html') 
+    if request.method== 'POST':
+        form =UserCreationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('usrname')
+            messages.success(request,f'welcome {username} your account is created')
+            return redirect('food:index')
+    else:    
+     form=UserCreationForm()
+    return render (request,'user/register.html',{'form':form})
+ 
